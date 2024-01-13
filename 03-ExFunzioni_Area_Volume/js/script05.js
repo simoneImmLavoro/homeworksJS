c651f6d160acce5ef0e1491b37ea09fb0457b5eb
simoneImmLavoro <simone.astore@edu.immaginazioneelavoro.it>
2024-01-13 11:03:29 +0100

    logic upgrade

--- 03-ExFunzioni_Area_Volume/js/script05.js
+++ 03-ExFunzioni_Area_Volume/js/script05.js
@@ -25,11 +25,20 @@ function calcolaVolume(area, profondità){
     return volume;
 }
 
+function checkValid(valore){
+    let nonValido = isNaN(valore) || valore === ""
+    return nonValido;
+}
+
 areaButton.addEventListener("click", function(){
     catchUserData();
     
-    if(isNaN(base) || base === "" || isNaN(altezza) || altezza === "" ){
+    if(checkValid(base) && checkValid(altezza)){
         risultati.innerHTML = "Per favore inserisci delle misure valide";
+    }else if(checkValid(base)){
+        risultati.innerHTML = "Inserisci un valore valido per la base";
+    }else if(checkValid(altezza)){
+        risultati.innerHTML = "Inserisci un valore valido per l'altezza";
     } else {
         risultati.innerHTML = `L'area è ${calcolaArea(base, altezza)}` 
     }
@@ -38,8 +47,21 @@ areaButton.addEventListener("click", function(){
 volumeButton.addEventListener("click", function(){
     catchUserData();
 
-    if(isNaN(base) || base === "" || isNaN(altezza) || altezza === "" || isNaN(profondita) || profondita === ""){
+
+    if((checkValid(base)) && checkValid(altezza) && checkValid(profondita)){
         risultati.innerHTML = "Per favore inserisci delle misure valide";
+    }else if((checkValid(base)) && checkValid(profondita)){
+        risultati.innerHTML = "Inserisci un valore valido per la base e uno per l'altezza";
+    }else if((checkValid(base)) && checkValid(profondita)){checkValid(altezza)
+        risultati.innerHTML = "Inserisci un valore valido per la base e uno per la profondità";
+    }else if(checkValid(altezza) && checkValid(profondita)){
+        risultati.innerHTML = "Inserisci un valore valido per l'altezza e uno per la profondità";
+    }else if(checkValid(base)){
+        risultati.innerHTML = "Inserisci un valore valido per la base";
+    }else if(checkValid(altezza)){
+        risultati.innerHTML = "Inserisci un valore valido per l'altezza";
+    }else if(checkValid(profondita)){
+        risultati.innerHTML = "Inserisci un valore valido per la profondità";
     } else {
         risultati.innerHTML = `Il volume è ${calcolaVolume(calcolaArea(base,altezza),profondita)}`;
     }
