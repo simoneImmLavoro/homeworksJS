d66f09ef2c49f8d6819327b3b9088038d161e1e8
simoneImmLavoro <simone.astore@edu.immaginazioneelavoro.it>
2024-01-21 14:14:48 +0100

    final

--- 05-ExAZZERAMENTO/js/script16.js
+++ 05-ExAZZERAMENTO/js/script16.js
@@ -1,10 +1,15 @@
-let array1 = [1,0,2,4,6];
+let array1 = [1,0,2,4,6,8];
 let array2 = [0,4,5,8,7];
 
 let finalArr = [];
 
-for(let i = 0; i < array1.length; i++){
-    let sum = array1[i] + array2[i];
+for(let i = 0; i < Math.max(array1.length, array2.length); i++){
+    
+    let value1 = array1[i] || 0;
+    let value2 = array2[i] || 0;
+
+
+    let sum = value1 + value2;
     finalArr.push(sum);
 }
 
