c37606e1ec7bdee1a41be2494651cb4090f5420f
simoneImmLavoro <simone.astore@edu.immaginazioneelavoro.it>
2024-01-27 17:19:46 +0100

    iwv

--- 08-Ex-ManipolazioneDOM/js/script07.js
+++ 08-Ex-ManipolazioneDOM/js/script07.js
@@ -1,1 +1,17 @@
-Math.PI
+let myRInput = document.querySelector("#raggio");
+let volumeBtn = document.querySelector("#volumeBtn");
+let resultField = document.querySelector("#result");
+
+function calcVol(raggio){
+    let volume = (4/3) * Math.PI * Math.pow(raggio, 3);
+    return volume;
+}
+
+
+volumeBtn.addEventListener("click", function(){
+    let myR = Number(myRInput.value);
+    let myVol = calcVol(myR);
+
+    resultField.innerHTML = "Il Volume della sfera con raggio " + myR + " è: " + myVol;
+    myRInput.value=""
+})
