ee20c05fd081faec044ef9874bf37f59b2aeb96d
simoneImmLavoro <simone.astore@edu.immaginazioneelavoro.it>
2024-02-27 22:47:18 +0100

    testing

--- 13-ExShop/js/script03.js
+++ 13-ExShop/js/script03.js
@@ -1,6 +1,9 @@
 let listaElementi = document.querySelector("#elListaProdotti");
+let linkCart = document.querySelector("#cartLink");
 
 let prodURL = "https:dummyjson.com/products";
+
+let mySavedProducts = [];
 
 fetch(prodURL)
     .then(data => {
@@ -8,7 +11,6 @@ fetch(prodURL)
     })
     .then( response => {
         let myProducts = response.products;
-        console.log(myProducts);
 
         myProducts.forEach(prodotto =>{
             creaItem(prodotto)
@@ -29,4 +31,25 @@ function creaItem(prodotto){
     listaElementi.appendChild(li)
     li.appendChild(buttCart)
     buttCart.appendChild(iconSpan)
+
+    let mioProdotto = prodotto;
+
+    buttCart.addEventListener("click", function(){
+        saveItem(mioProdotto);
+    });
 }
+
+function saveItem(mioProdotto){
+    mySavedProducts.push(mioProdotto);
+    console.log(mySavedProducts);
+}
+
+function saveCartList(){
+    let myListOBJ = JSON.stringify(mySavedProducts);
+    localStorage.setItem("cartList", myListOBJ)
+}
+
+linkCart.addEventListener("click", function(){
+    saveCartList()
+})
+
